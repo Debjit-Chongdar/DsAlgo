4d0bbcfc6def497366c48da197c8af8d28abd899
Debjit <debjit.chongdar@gmail.com>
2025-06-16 23:13:33 +0530

    kruskals Algo and prims algo

---
+++ src/graph/kruskals_algo/MinCostToConnectAllPoint.java
@@ -0,0 +1,79 @@
+package graph.kruskals_algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinCostToConnectAllPoint {
+    public static int kruskals_algo(int[][] vertices){
+        List<int[]> edgesWithCost = populateEdgesWithCost(vertices);
+        edgesWithCost.sort((a, b)-> a[2]-b[2]);
+        //Set<Integer> visited = new HashSet<>();
+        UnionFind uf = new UnionFind(vertices.length);
+        int totalCost = 0;
+        for(int[] edgeNcost : edgesWithCost){
+            int vertex1 = edgeNcost[0];
+            int vertex2 = edgeNcost[1];
+            int cost = edgeNcost[2];
+            /*if(!visited.contains(vertex1) || !visited.contains(vertex2)){
+                visited.add(vertex1);
+                visited.add(vertex2);
+                totalCost += cost;
+            }*/
+            if(uf.union(vertex1, vertex2)){
+                totalCost += cost;
+            }
+        }
+        return totalCost;
+    }
+
+    private static List<int[]> populateEdgesWithCost(int[][] vertices) {
+        List<int[]> result = new ArrayList<>();
+        for(int i=0; i<vertices.length; i++){
+            int x1 = vertices[i][0];
+            int y1 = vertices[i][1];
+            for(int j=i+1; j<vertices.length; j++){
+                int x2 = vertices[j][0];
+                int y2 = vertices[j][1];
+                int cost = Math.abs(x1-x2) + Math.abs(y1-y2);
+                result.add(new int[]{i, j, cost});
+            }
+        }
+        return result;
+    }
+    static class UnionFind{
+        private int[] parent,rank;
+        public UnionFind(int n){
+            parent = new int[n];
+            rank = new int[n];
+            for(int i=0; i<n; i++){
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+        public int find(int n){
+            if(n != parent[n]){
+                parent[n] = find(parent[n]);
+            }
+            return parent[n];
+        }
+        public boolean union(int u, int v){
+            int up = find(u);
+            int vp = find(v);
+            if(up == vp){
+                return false;
+            }
+            if(rank[up] > rank[vp]){
+                parent[vp] = up;
+                rank[up] = rank[up] + rank[vp];
+            }else{
+                parent[up] = vp;
+                rank[vp] = rank[vp] + rank[up];
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("expected 10 # "+kruskals_algo(new int[][] {{0,0},{2,2},{3,3},{2,4},{4,2}}));
+    }
+}
---
+++ src/graph/prim_algo/MinCostToConnectAllPoint.java
@@ -0,0 +1,59 @@
+package graph.prim_algo;
+
+import java.util.*;
+
+// We have few vertices position, which can connect each other, find the smallest edge to connect all vertices
+// difference between two vertices is the cost/weight of the edge
+//[[0,0],[2,2],[3,3],[2,4],[4,2]]
+public class MinCostToConnectAllPoint {
+    public static int primAlgo(int[][] vertices) {
+        Map<Integer, List<int[]>> adjList = populateAdjacencyList(vertices);
+        PriorityQueue<int[]> minHeapOnCost = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        minHeapOnCost.offer(new int[]{0, 0});
+        int cost = 0;
+        Set<Integer> visitedVertices = new HashSet<>();
+        while (visitedVertices.size() < vertices.length) {
+            int[] vertexCost = minHeapOnCost.poll();
+            int vertex = vertexCost[0];
+            int curCost = vertexCost[1];
+            if (visitedVertices.contains(vertex)) {
+                continue;
+            }
+            visitedVertices.add(vertex);
+            cost += curCost;
+            for (int[] neighbour : adjList.get(vertex)) {
+                if (!visitedVertices.contains(neighbour[0])) { // this check is optional as we are checking before
+                    minHeapOnCost.offer(neighbour);
+                }
+            }
+        }
+        return cost;
+    }
+
+    private static Map<Integer, List<int[]>> populateAdjacencyList(int[][] vertices) {
+        Map<Integer, List<int[]>> adjList = new HashMap<>();
+        for (int i = 0; i < vertices.length; i++) {
+            int x1 = vertices[i][0]; //x axis
+            int y1 = vertices[i][1]; // y axis
+            for (int j = i + 1; j < vertices.length; j++) {
+                int x2 = vertices[j][0];
+                int y2 = vertices[j][1];
+                int cost = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+                adjList.computeIfAbsent(i, key -> new ArrayList<>()).add(new int[]{j, cost});
+                adjList.computeIfAbsent(j, key -> new ArrayList<>()).add(new int[]{i, cost});
+            }
+        }
+        return adjList;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("expected 10 # " + primAlgo(new int[][]{{0, 0}, {2, 2}, {3, 3}, {2, 4}, {4, 2}}));
+    }
+}
+//                4
+//                !
+//                !
+//  0 ------------1 ------ 2
+//                !
+//                !
+//                3
