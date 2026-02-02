434e2fafadae5bc1dc1a26b5aaf46185d2bd2ce5
Debjit <debjit.chongdar@gmail.com>
2026-02-02 21:51:04 +0530

    stream and Lambda expression

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.util.*;
+import java.util.stream.Collectors;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Order{
+    int countryCode;
+    String state;
+    int order_count;
+    public int getOrder_count(){
+        return this.order_count;
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public int getCountryCode() {
+        return countryCode;
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        List<List<Order>> countryOrders = new ArrayList<>();
+        Map<Integer, List<Order>> result = countryOrders.stream().flatMap(list-> list.stream())
+                .collect(Collectors.groupingBy(Order::getCountryCode,
+                        Collectors.collectingAndThen(Collectors.toList(),
+                                list -> list.stream()
+                                        .sorted(Comparator.comparingInt(Order::getOrder_count)
+                                                .reversed()).limit(3).collect(Collectors.toList())
+                        )
+                ));
+
+        List<List<Order>> result_1 = countryOrders.stream().map(
+                list -> list.stream().sorted(
+                        Comparator.comparingInt(Order::getOrder_count).reversed()
+                ).skip(2).limit(3).collect(Collectors.toList())
+        ).collect(Collectors.toList());
+    }
+}
+// Skip => it will skip first number of elements mentioned
+// limit => how many we want in response
